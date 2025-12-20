d4699ef5910c3ca47591799507cde8e05fb2f5f2
dspeckman76 <d.speckman76@gmail.com>
2025-12-20 16:50:16 -0600

    Dec 20 Update 2

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 //OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=48fa60c3
 
 const key = '48fa60c3';
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const favoritesBtn = document.getElementById("favoritesBtn");
+const homeBtn = document.getElementById("homeBtn");
+
+async function main() {
+console.log(await(await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=48fa60c3")).json());
+}
+main();
